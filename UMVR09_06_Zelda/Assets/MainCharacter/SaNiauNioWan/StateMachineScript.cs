f58179431656c07e4acbeed7a2c96af8750d261b
Joey Liu <joey@ntsoap.com>
2022-11-09 15:20:04 +0800

    Add StateMachineScript

---
+++ UMVR09_06_Zelda/Assets/MainCharacter/SaNiauNioWan/StateMachineScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachineScript : MonoBehaviour
+{
+    public Animator animator;
+    public AnimatorStateInfo asi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        asi = animator.GetCurrentAnimatorStateInfo(0);
+        var tpc = this.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter>();
+        if (asi.IsName("attack01") || asi.IsName("attack01 0") || asi.IsName("attack01 1") || asi.IsName("attack01 2") || asi.IsName("getHit") || asi.IsName("died"))
+            tpc.CanRotate = false;
+        else tpc.CanRotate = true;
+        //if(asi.IsName(nowAsiName))
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            animator.SetTrigger("died");
+            //animator.SetFloat("hp", 0.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            animator.SetTrigger("getHit");
+        }
+
+        if (animator.IsInTransition(0) == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+                LeftMouseClick();
+            if (Input.GetMouseButtonDown(1))
+                RightMouseClick();
+        }
+    }
+    public virtual void LeftMouseClick()
+    {
+        animator.SetTrigger("attack01");
+    }
+    public virtual void RightMouseClick()
+    {
+        animator.SetTrigger("attack02");
+    }
+
+}
