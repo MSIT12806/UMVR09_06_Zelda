fa7dc30ab4653ab2b179235a617202458c4c30cf
Robin <alegalone@gmail.com>
2022-11-15 15:40:15 +0800

    敵人被攻擊的動作切換

---
+++ UMVR09_06_Zelda/Assets/Script/Npc/AiStateMachine.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AiStateMachine 
+{
+   
+}
+
+//警戒巡邏 //地域性
+
+//攻擊
+//1. 腳色進入領地 2. Npc 看見腳色
+
+//追進
+//對峙
+//逃開
+
+//受攻僵直1
+//受攻僵直2
+//受攻僵直3
+//受攻僵直4
+
+//擊倒
+//起身
