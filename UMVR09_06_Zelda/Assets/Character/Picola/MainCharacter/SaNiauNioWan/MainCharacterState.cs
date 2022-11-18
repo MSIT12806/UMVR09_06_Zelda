41a21b15f7e840f19c432c53d1a9eb3a1b64310b
robin <alegalone@gmail.com>
2022-11-18 11:55:52 +0800

    改名

--- UMVR09_06_Zelda/Assets/Character/Picola/MainCharacter/SaNiauNioWan/MainCharacterState.cs
+++ UMVR09_06_Zelda/Assets/Character/Picola/MainCharacter/SaNiauNioWan/MainCharacterState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateMachineScript : MonoBehaviour
+public class MainCharacterState : MonoBehaviour
 {
     public GameObject Sword;
     public Animator animator;
