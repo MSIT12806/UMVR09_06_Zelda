3479dfcec19ece68a227286ab9a312b12e88d713
robin <alegalone@gmail.com>
2022-11-18 14:53:48 +0800

    ai temp

--- UMVR09_06_Zelda/Assets/Script/Npc/NpcManager.cs
+++ UMVR09_06_Zelda/Assets/Script/Npc/NpcManager.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public static class NpcManager 
 {
+    public static List<GameObject> Npcs = new List<GameObject>();
     //處理 npc 碰撞、偵測、迴避、群體運動等行為。
+    //chase: 檢查目前會攻擊玩家的角色有幾人，並適時切換 npc 狀態為 around or close。
+
+
 }
