1530745d4d3dfc0f136113b7542b92dc389f6d28
HsuTsaiWei <w820721@gmail.com>
2022-11-22 02:49:17 +0800

    戰鬥動作修正參考完成、加入投擲動作與投擲物

--- UMVR09_06_Zelda/Assets/Script/Throw.cs
+++ UMVR09_06_Zelda/Assets/Script/Throw.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponRotate : MonoBehaviour
+public class Throw : MonoBehaviour
 {
-    public float WeaponRotateAngle = 90.0f;
-
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(0, WeaponRotateAngle, 0);
+        
     }
 
     // Update is called once per frame
