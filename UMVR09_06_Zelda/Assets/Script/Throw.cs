a288750053c9eb4b4eee406d56023904077fef81
HsuTsaiWei <w820721@gmail.com>
2022-11-26 22:45:04 +0800

    道具使用動作，作業中

--- UMVR09_06_Zelda/Assets/Script/Throw.cs
+++ UMVR09_06_Zelda/Assets/Script/Throw.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Throw : MonoBehaviour
 {
+    private enum Item 
+    {
+        Apple,
+        TimeStop,
+        Ice,
+        Bomb,
+    }
+
     private Vector3 start_pos;
     private Vector3 current_pos;
     private Vector3 start_vel;
@@ -12,7 +20,9 @@ public class Throw : MonoBehaviour
     private Vector3 resistance;
 
     private Vector3 face;
+    private bool CanThrow = true;
     private bool isThrowing = false;
+    private Item useItem;
 
     private Transform ThrowItem;
     public Transform RightHandThrow_pos;
@@ -21,17 +31,69 @@ public class Throw : MonoBehaviour
     public float vertical = 0.2f;
     private Vector3 Gravity = new Vector3(0,-1,0);
 
+    Animator animator;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (isThrowing == true) 
+        if (CanThrow==true) 
         {
-            OnThrow();
+            GetThrowKeyIn();
+
+            switch (useItem) 
+            {
+                case Item.TimeStop:
+                    useTimeStop();
+                    break;
+                case Item.Ice:
+                    useIce();
+                    break;
+                case Item.Bomb:
+                    useBomb();
+                    break;
+            }
         }
+        OnThrow();
     }
+
+    void GetThrowKeyIn() 
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha2)) useItem = Item.TimeStop;
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) useItem = Item.Ice;
+        else if (Input.GetKeyDown(KeyCode.Alpha4)) useItem = Item.Bomb;
+    }
+
+    void useTimeStop() 
+    {
+        animator.SetTrigger("ThrowTimeStop");
+        CanThrow = false;
+    }
+
+    void useIce() 
+    {
+        animator.SetTrigger("ThrowIce");
+        CanThrow = false;
+    }
+
+    void useBomb() 
+    {
+        animator.SetTrigger("ThrowBomb");
+        CanThrow = false;
+    }
+
     public void SwordFalse()
     {
         Sword.SetActive(false);
+    }
+    public void SwordTrue()
+    {
+        Sword.SetActive(true);
     }
 
     public void GetBomb()
@@ -52,9 +114,17 @@ public class Throw : MonoBehaviour
         ThrowItem = go.transform;
     }
 
+    public void GetTimeStop()
+    {
+        Object o = Resources.Load("TranslucentCrystal_TimeStop");
+        GameObject go = Instantiate((GameObject)o);
+        go.transform.SetParent(RightHandThrow_pos.transform);
+        go.transform.position = RightHandThrow_pos.position;
+        ThrowItem = go.transform;
+    }
+
     public void StartThrow() 
     {
-        isThrowing = true;
         ThrowItem.transform.parent = null;
 
         face = transform.forward;
@@ -74,20 +144,25 @@ public class Throw : MonoBehaviour
 
         //阻力 = - 初始速度 * 0.X
         resistance = -(vel) * 0.05f;
+
+        isThrowing = true;
     }
 
     public void OnThrow() 
     {
-        //下一位置 = 當前位置 + 當前速度
-        //當前位置 = 下一位置
-        current_pos = ThrowItem.position + vel;
-        //物體移動到當前位置
-        ThrowItem.position = current_pos;
+        if (isThrowing)
+        {
+            //下一位置 = 當前位置 + 當前速度
+            //當前位置 = 下一位置
+            current_pos = ThrowItem.position + vel;
+            //物體移動到當前位置
+            ThrowItem.position = current_pos;
 
-        //下一速度 = 當前速度 + 重力 * Time.Deltatime + 阻力 * Time.Deltatime
-        next_vel = vel + Gravity * Time.deltaTime + resistance * Time.deltaTime;
-        //當前速度 = 下一速度
-        vel = next_vel;
+            //下一速度 = 當前速度 + 重力 * Time.Deltatime + 阻力 * Time.Deltatime
+            next_vel = vel + Gravity * Time.deltaTime + resistance * Time.deltaTime;
+            //當前速度 = 下一速度
+            vel = next_vel;
+        }
     }
     public void EndThrow()
     {
@@ -98,19 +173,30 @@ public class Throw : MonoBehaviour
 
 
     /*
-    初始位置 = 物件生成位置
-    當前位置 = 初始位置
-    初始速度 = 人物面向 * 速率
-    阻力 = - 初始速度 * 0.X
-    當前速度 = 初始速度
+    
+    if (CanThrow)
+    {
+        switch(按鍵)
+        {
+        case Alpha.2:時停
+        執行時停()
+        case Alpha.3:冰
+        執行冰()
+        case Alpha.2:炸彈
+        執行炸彈()
+        }
+    }
 
-    update
+    讀取按鍵()
+    CD計時器()
 
-    下一位置 = 當前位置 + 當前速度
-    當前位置 = 下一位置
-    下一速度 = 當前速度 + 重力 * Time.Deltatime - 阻力 * Time.Deltatime 
-    當前速度 = 下一速度
-
+    時停()
+    {
+    IK關掉
+    道具進入CD
+    UI操作
+    怪物反應
+    }
      */
 
 }
