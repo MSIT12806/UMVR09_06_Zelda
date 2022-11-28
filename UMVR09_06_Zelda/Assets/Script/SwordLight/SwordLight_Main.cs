40b1379e56e78c5c216479d5060052fbc78b9e56
HsuTsaiWei <w820721@gmail.com>
2022-11-29 03:58:11 +0800

    刀光測試 & 特效下載

---
+++ UMVR09_06_Zelda/Assets/Script/SwordLight/SwordLight_Main.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordLight_Main : MonoBehaviour
+{
+    public GameObject AttackEffect01;
+
+    private Animation anim;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if(stateInfo.IsName("Attack01") && stateInfo.normalizedTime < 0.5f)
+        {
+                AttackEffect01.GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
