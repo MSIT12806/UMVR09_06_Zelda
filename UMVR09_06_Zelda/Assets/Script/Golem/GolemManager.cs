e4a8f6f8488c27ba0f10b82c30d81c9f25df400c
Joey Liu <joey@ntsoap.com>
2022-11-30 10:20:50 +0800

    golem manager

--- UMVR09_06_Zelda/Assets/Script/Golem/GolemManager.cs
+++ UMVR09_06_Zelda/Assets/Script/Golem/GolemManager.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GolemManager : MonoBehaviour
+public class GolemManager : MonoBehaviour,IHp
 {
     AiState aiState;
+    Npc npc;
+    public float Hp { get => npc.Hp; set => npc.Hp = value; }
+
+    void Awake()
+    {
+        print(GetInstanceID());
+        ObjectManager.StateManagers.Add(this.gameObject.GetInstanceID(), this);
+    }
     // Start is called before the first frame update
     void Start()
     {
         aiState = new GolemIdleState(ObjectManager.MainCharacter, transform.GetComponent<Animator>(), transform, 50f);
+        npc = transform.GetComponent<Npc>();
     }
 
     // Update is called once per frame
