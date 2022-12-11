990df66c34fa768b58634fbe57c9aeed429d03ec
STUDENT-PC\Student <w820721@gmail.com>
2022-12-11 09:57:18 +0800

    刪除：暫放雜兵 修改：重擊特效

--- UMVR09_06_Zelda/Assets/Script/Throw.cs
+++ UMVR09_06_Zelda/Assets/Script/Throw.cs
@@ -292,7 +292,7 @@ public class Throw : MonoBehaviour
         }
         else if(Explode == "FX_TimeStop")
         {
-            NpcCommon.AttackDetection("Pico", itemEffect_pos, ItemEffect_obj.transform.forward, 360.0f, 2.7f, false, new DamageData(10, Vector3.zero, HitType.Heavy, new DamageStateInfo(DamageState.TimePause, 5)), "Npc");
+            NpcCommon.AttackDetection("Pico", itemEffect_pos, ItemEffect_obj.transform.forward, 360.0f, 5.0f, false, new DamageData(10, Vector3.zero, HitType.Heavy, new DamageStateInfo(DamageState.TimePause, 5)), "Npc");
             ObjectManager.TimeStopChain.transform.position = itemEffect_pos;
             ObjectManager.TimeStopChain.SetActive(true);
             Invoke("ChainDisable", 5);
