296c4706ac426bc334e6d49df8e72c7413c251d1
Kinnyu <joey@ntsoap.com>
2022-12-14 23:20:13 +0800

    測量Space瞬移範圍

--- UMVR09_06_Zelda/Assets/Script/Npc/Space/SpaceMoveBehavior.cs
+++ UMVR09_06_Zelda/Assets/Script/Npc/Space/SpaceMoveBehavior.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpaceMoveBehavior : StateMachineBehaviour
 {
+    Transform selfTransform;
     Transform target;
     bool awake;
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -11,13 +12,14 @@ public class SpaceMoveBehavior : StateMachineBehaviour
         if (awake == false)
         {
             target = ObjectManager.MainCharacter;
+            selfTransform = animator.transform;
             awake = true;
         }
     }
 
     public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       
+
     }
 
     /// <summary>
@@ -29,5 +31,22 @@ public class SpaceMoveBehavior : StateMachineBehaviour
         //1. 不能順移出場地
         //2. 不能卡住或無限墜落
         //3. 不能讓 Lico 打不到的位置
+        Vector3 NewPos;
+        NewPos = selfTransform.position;
+        NewPos.x = RandomFloat(TeleportSpace.Point4.x ,TeleportSpace.Point2.x);
+        NewPos.z = RandomFloat(TeleportSpace.Point4.z, TeleportSpace.Point2.z);
+
+        selfTransform.position = NewPos;
+
+    }
+    public float RandomFloat(float min, float max)
+    {
+        System.Random random = new System.Random();
+        double val = (random.NextDouble() * (max - min) + min);
+        return (float)val;
+    }
+    public void CanTp()
+    {
+
     }
 }
---
+++ UMVR09_06_Zelda/Assets/Script/Scene2CubeDrawLine.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene2CubeDrawLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 point1 = transform.position + transform.forward * 10f + transform.right * -10f;
+        Vector3 point2 = point1 + transform.right * 20f;
+        Vector3 point3 = point2 + transform.forward * -20f;
+        Vector3 point4 = point3 + transform.right * -20f;
+        Debug.DrawLine(point1, point2, Color.red);
+        Debug.DrawLine(point2, point3, Color.blue);
+        Debug.DrawLine(point3, point4, Color.green);
+        Debug.DrawLine(point4, point1, Color.green);
+
+        TeleportSpace.Point1 = point1;
+        TeleportSpace.Point2 = point2;
+        TeleportSpace.Point3 = point3;
+        TeleportSpace.Point4 = point4;
+
+        //Debug.DrawLine(transform.position, transform.position + transform.forward * 10f, Color.green);
+        //Debug.DrawLine(transform.position, transform.position + transform.forward * -10f, Color.green);
+        //Debug.DrawLine(transform.position, transform.position + transform.right * 10f, Color.green);
+        //Debug.DrawLine(transform.position, transform.position + transform.right * -10f, Color.green);
+    }
+}
+public static class TeleportSpace
+{
+    public static Vector3 Point1;
+    public static Vector3 Point2;
+    public static Vector3 Point3;
+    public static Vector3 Point4;
+    //space.posision > point4
+    //space.posision < point2
+}
