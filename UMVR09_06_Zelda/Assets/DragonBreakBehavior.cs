ba8e66765b7da110b9c3f55a20f2c3fa54bd3038
robin <alegalone@gmail.com>
2022-12-14 16:43:49 +0800

    莉可(主角)  終結技可重複施放的 bug

--- UMVR09_06_Zelda/Assets/DragonBreakBehavior.cs
+++ UMVR09_06_Zelda/Assets/DragonBreakBehavior.cs
@@ -20,6 +20,8 @@ public class DragonBreakBehavior : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        var mcs = ObjectManager.MainCharacter.GetComponent<MainCharacterState>();
+        mcs.FinishingReleased = false;
         manager.ResetWeakPoint();
     }
 
