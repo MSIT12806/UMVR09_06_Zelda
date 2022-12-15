1a79cf78d4d1da46e29716d5cc59ad1efce591db
robin <alegalone@gmail.com>
2022-12-15 16:45:00 +0800

    場景二UI

--- UMVR09_06_Zelda/Assets/Script/GameManager/StageManager.cs
+++ UMVR09_06_Zelda/Assets/Script/GameManager/StageManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StageManager : MonoBehaviour
 {
@@ -9,18 +10,36 @@ public class StageManager : MonoBehaviour
     public float distance = 10;
     PicoState picoState;
     int stageOneWave;
-     DragonManager Dragon;
-     GolemManager Golem;
+    DragonManager Dragon;
+    GolemManager Golem;
+
+    bool isNightScene;
     // Start is called before the first frame update
     void Start()
     {
-        Dragon = GameObject.Find("Blue Variant").GetComponent<DragonManager>();
-        Golem = GameObject.Find("PBR_Golem (1)").GetComponent<GolemManager>();
+        var currentScene = SceneManager.GetActiveScene();
+        var currentSceneName = currentScene.name;
+        if (currentSceneName == "NightScene")
+        {
+            isNightScene = true;
+        }
+        else
+        {
+            isNightScene = false;
+        }
+
+
+
+
         picoState = Pico.GetComponent<PicoState>();
         if (TriggerType == 1)
         {
             stageOneWave = 4;//第一關會有四波
         }
+
+
+        Dragon = GameObject.Find("Blue Variant").GetComponent<DragonManager>();
+        Golem = GameObject.Find("PBR_Golem (1)").GetComponent<GolemManager>();
     }
 
     // Update is called once per frame
@@ -30,31 +49,40 @@ public class StageManager : MonoBehaviour
         if (d <= distance)
         {
             picoState.gameState = (GameState)TriggerType;
-            ObjectManager.myCamera.stage = TriggerType;
+            if (isNightScene)
+                ObjectManager.myCamera.stage = TriggerType;
+            else
+                ObjectManager2.myCamera.stage = TriggerType;
         }
 
-        switch (TriggerType)
+        if (isNightScene)
         {
-            case 1:
-                if (stageOneWave > 0 && ObjectManager.StageMonsterMonitor[1] < 10)
-                {
-                    print("?????");
-                    ObjectManager.StageOneResurrection();
-                    stageOneWave--;
-                }
-                return;
-            case 2:
-                if (Dragon.Hp > 0 && ObjectManager.StageMonsterMonitor[2] < 10)
-                {
-                    ObjectManager.StageTwoResurrection();
-                }
-                return;
-            case 3:
-                if (Golem.Hp > 0 && ObjectManager.StageMonsterMonitor[3] < 10)
-                {
-                    ObjectManager.StageThreeResurrection();
-                }
-                return;
+            switch (TriggerType)
+            {
+                case 1:
+                    if (stageOneWave > 0 && ObjectManager.StageMonsterMonitor[1] < 10)
+                    {
+                        ObjectManager.StageOneResurrection();
+                        stageOneWave--;
+                    }
+                    return;
+                case 2:
+                    if (Dragon.Hp > 0 && ObjectManager.StageMonsterMonitor[2] < 10)
+                    {
+                        ObjectManager.StageTwoResurrection();
+                    }
+                    return;
+                case 3:
+                    if (Golem.Hp > 0 && ObjectManager.StageMonsterMonitor[3] < 10)
+                    {
+                        ObjectManager.StageThreeResurrection();
+                    }
+                    return;
+            }
+        }
+        else
+        {
+
         }
     }
 
