bf7d98aa2004e55a90289bef88604ed71be347b7
robin <alegalone@gmail.com>
2022-12-15 10:39:31 +0800

    場景2 遊戲管理者；阿蘭娜行為機

---
+++ UMVR09_06_Zelda/Assets/Script/GameManager/ObjectManager2.cs
@@ -0,0 +1,43 @@
+using Ron;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UIElements;
+
+public class ObjectManager2 : MonoBehaviour
+{
+    public static Dictionary<int, GameObject> NpcsAlive;//碰撞偵測、攻擊判定用。
+    public static List<GameObject> Statics;
+    public static Queue<GameObject> AttackFx;
+    public static TPSCamera myCamera;
+    public static Transform MainCharacter;
+    public static Transform MainCharacterHead;
+    public static GameObject TimeStopChain;
+    public Transform MyCharacter;
+    public static int[] StageMonsterMonitor { get; } = new int[4];
+
+
+
+
+    //    public static List<GameObject> UsaoResources;
+    //處理 npc 碰撞、偵測、迴避、群體運動等行為。
+    //chase: 檢查目前會攻擊玩家的角色有幾人，並適時切換 npc 狀態為 around or close。
+    private void Awake()
+    {
+        MainCharacter = MyCharacter;
+        MainCharacterHead = MainCharacter.FindAnyChild<Transform>("Head");
+
+      
+        var meshs = transform.GetComponentsInChildren<MeshRenderer>();
+        foreach (var item in meshs)
+        {
+            item.enabled = false;
+        }
+
+    }
+
+}
---
+++ UMVR09_06_Zelda/Assets/Script/Npc/Space/SpaceFightBehavior.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceFightBehavior : StateMachineBehaviour
+{
+    Transform target;
+    bool awake;
+    float dazeSeconds;
+    float weight;
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (awake == false)
+        {
+            target = ObjectManager2.MainCharacter;
+            awake = true;
+        }
+        dazeSeconds = UnityEngine.Random.value * 3;
+        weight = UnityEngine.Random.value;
+    }
+
+    public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+        AiStateCommon.Turn(animator.transform, (target.position - animator.transform.position).normalized);
+        dazeSeconds -= Time.deltaTime;
+        if(dazeSeconds <= 0)
+        {
+            animator.SetTrigger("Move");
+        }
+
+        var distance = Vector3.Distance(animator.transform.position, target.position);
+
+        if (distance <= 15 && weight > 0.8)
+        {
+            animator.SetTrigger("Attack01");//15m內都可以追蹤球
+        }
+        else if(distance<=10 && weight > 0.8)
+        {
+            animator.SetTrigger("Attack03");//10m內可以發射氣功
+        }
+        else if (distance <= 6)
+        {
+            animator.SetTrigger("Attack02");//6m內爆炸？
+        }
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+    }
+}
