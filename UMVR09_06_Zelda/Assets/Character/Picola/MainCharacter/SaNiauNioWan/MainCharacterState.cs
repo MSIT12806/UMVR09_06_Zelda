698926aec00c180da169e87978b3b39cea9c163f
robin <alegalone@gmail.com>
2022-12-16 20:38:20 +0800

    讓終結技好好面對施放對象

--- UMVR09_06_Zelda/Assets/Character/Picola/MainCharacter/SaNiauNioWan/MainCharacterState.cs
+++ UMVR09_06_Zelda/Assets/Character/Picola/MainCharacter/SaNiauNioWan/MainCharacterState.cs
@@ -304,6 +304,7 @@ public class MainCharacterState : MonoBehaviour, NpcHelper
                 {
                     managers = ObjectManager2.StateManagers;
                 }
+                transform.forward = i.transform.position - transform.position;
                 var nh = managers[i.GetInstanceID()];
                 if (nh.WeakPoint <=0)
                 {
@@ -357,7 +358,7 @@ public class MainCharacterState : MonoBehaviour, NpcHelper
         if (attackType == 3)//重擊2
             NpcCommon.AttackDetection("Pico", transform.position + transform.forward * -0.2f, transform.forward, 180, 4.5f, true, new DamageData(20f, transform.forward * 0.15f, HitType.Heavy, DamageStateInfo.NormalAttack), "Npc");
         if (attackType == 4)//終結技
-            NpcCommon.AttackDetection("Pico", transform.position, transform.forward, 180, 4f, true, new DamageData(100f, transform.forward * 0.15f, HitType.finishing, DamageStateInfo.NormalAttack), "Npc");
+            NpcCommon.AttackDetection("Pico", transform.position, transform.forward, 360, 4f, true, new DamageData(100f, transform.forward * 0.15f, HitType.finishing, DamageStateInfo.NormalAttack), "Npc");
         if (attackType == 5)//無雙
             NpcCommon.AttackDetection("Pico", transform.position, transform.forward, 360, 13f, true, new DamageData(200f, transform.forward * 0.15f, HitType.Heavy, new DamageStateInfo(DamageState.Fever, 0)), "Npc");
 
