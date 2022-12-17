e2712bc259a5405cd3a04658057e93cd614571a8
Kinnyu <joey@ntsoap.com>
2022-12-17 22:23:40 +0800

    space黑洞技

--- UMVR09_06_Zelda/Assets/Script/Npc/Space/SpaceSkill030Behavior.cs
+++ UMVR09_06_Zelda/Assets/Script/Npc/Space/SpaceSkill030Behavior.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class SpaceSkill030Behavior : StateMachineBehaviour
 {
+
+    Transform target;
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        target = ObjectManager2.MainCharacter.transform;
         UiManager.singleton.ShowSikaTip("ItemIceTips");
+    }
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.transform.LookAt(target);
+    }
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        UiManager.singleton.HideTip();
     }
 }
