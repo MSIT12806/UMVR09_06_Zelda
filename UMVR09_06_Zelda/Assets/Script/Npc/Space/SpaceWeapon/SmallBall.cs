d58180c9274aad0f65da1d19e8eb769b3175661d
robin <alegalone@gmail.com>
2022-12-19 18:28:51 +0800

    加上地形 layer 、復原小球script

--- UMVR09_06_Zelda/Assets/Script/Npc/Space/SpaceWeapon/SmallBall.cs
+++ UMVR09_06_Zelda/Assets/Script/Npc/Space/SpaceWeapon/SmallBall.cs
@@ -5,21 +5,67 @@ using UnityEngine;
 public class SmallBall : MonoBehaviour
 {
     // Start is called before the first frame update
-    Transform target;
-    const float initVelocity = 12f;
+    float speedPerSecond = 8.4f;
+    readonly float Radius = 1f;
+    readonly float Angle = 360f;
+    readonly float attackSeconds = 4f;
+    float nowAttackSecond;
     void Start()
     {
-        target = ObjectManager2.MainCharacter;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (target == null) target = ObjectManager2.MainCharacter;
-
-        target.forward = (target.position - transform.position).normalized;
-        transform.position += target.forward * initVelocity * Time.deltaTime;
+        if (nowAttackSecond > 0)
+        {
+            nowAttackSecond -= Time.deltaTime;
+            transform.forward = attackDirection;
+            AttackBehavior();
+            return;
+        }
+        AroundBehavior();
     }
 
+    void AroundBehavior()
+    {
+        attackDirection = (ObjectManager2.MainCharacter.position.WithY() - transform.position.WithY()).normalized;
 
+        var distance = Vector3.Distance(transform.position.WithY(), ObjectManager2.Elena.position.WithY());
+        var directionFaceSpace = (ObjectManager2.Elena.position.WithY() - transform.position.WithY()).normalized;
+        if (distance >= Radius + 0.1)
+        {
+            transform.forward = directionFaceSpace;
+            transform.position += transform.forward * speedPerSecond * Time.deltaTime;
+            return;
+        }
+
+        if (distance <= Radius - 0.1)
+        {
+            transform.forward = -directionFaceSpace;
+            transform.position += transform.forward * speedPerSecond * Time.deltaTime;
+            return;
+        }
+
+        transform.RotateAround(ObjectManager2.Elena.position, ObjectManager2.Elena.up, Time.deltaTime * Angle);
+
+    }
+    Vector3 attackDirection;
+    void AttackBehavior()
+    {
+
+        transform.forward += ((ObjectManager2.MainCharacter.position + ObjectManager2.MainCharacter.forward) - transform.position).normalized;//
+        transform.position += transform.forward * speedPerSecond * Time.deltaTime;
+        //
+        //1.以Space為圓心
+        //2.以lico 為半徑
+        //3.進行旋轉
+        //預計攻擊4秒
+    }
+
+    public void Attack()
+    {
+        nowAttackSecond = attackSeconds;
+    }
 }
