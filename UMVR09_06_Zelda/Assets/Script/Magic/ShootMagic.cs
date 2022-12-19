56ec5af8938cfe6248d6ca828934495e0c6fc57b
robin <alegalone@gmail.com>
2022-12-19 18:22:31 +0800

    魔王 發球攻擊

--- UMVR09_06_Zelda/Assets/Script/Magic/ShootMagic.cs
+++ UMVR09_06_Zelda/Assets/Script/Magic/ShootMagic.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 
 public class ShootMagic : MonoBehaviour
 {
-    public Vector3 force;
-    public float existSeconds;
+    [HideInInspector] public Vector3 force;
+    [HideInInspector] public float existSeconds;
+    public float speedDecrease = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +27,7 @@ public class ShootMagic : MonoBehaviour
             offset = 2f;
         }
         force += (ObjectManager.MainCharacter.position + (ObjectManager.MainCharacter.forward - (ObjectManager.MainCharacter.position - transform.position).normalized) * offset - transform.position).WithY(-1.5f).normalized * 5;
-        transform.Translate(force.normalized / 5f);
+        transform.Translate(force.normalized / speedDecrease);
         existSeconds -= Time.deltaTime;
         bool land = Physics.Raycast(transform.position, -Vector3.up, out var hit, 0.3f, terrainMask);
         if (existSeconds <= 0 || Vector3.Distance(transform.position, ObjectManager.MainCharacter.position.AddY(1)) < 0.4 || land)
