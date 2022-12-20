094ca90e024dc80ba99bdcb8f86111a8eff53a88
robin <alegalone@gmail.com>
2022-12-20 15:34:58 +0800

    lico 受傷聲音 Awake 估調

--- UMVR09_06_Zelda/Assets/Script/Npc/Space/SpaceWeapon/BigBallExplosion.cs
+++ UMVR09_06_Zelda/Assets/Script/Npc/Space/SpaceWeapon/BigBallExplosion.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class BigBallExplosion : MonoBehaviour
+public class SmallBallExplosion : MonoBehaviour
 {
     ParticleSystem keeping;
     public ParticleSystem[] explosions;
